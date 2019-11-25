969d695cb08e0db3c5286f1940a6e3afa8703114
Kevin Nobel <hi@2sk.nl>
2019-11-25 18:54:16 +0100

    Updated scraper to work with a more general approach.

--- src/main/java/com/tweska/sweetheads/scraper/MinecraftHeadsScraper.java
+++ src/main/java/com/tweska/sweetheads/scraper/MinecraftHeadsScraper.java
@@ -1,5 +1,7 @@
 package com.tweska.sweetheads.scraper;
 
+import com.tweska.sweetheads.heads.Head;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,37 +9,64 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import java.util.logging.Logger;
 
-public abstract class MinecraftHeadsScraper implements Scraper {
+public class MinecraftHeadsScraper implements Scraper {
+    private static final String source = "Minecraft-Heads.com";
 
-    private static final String base = "https://minecraft-heads.com";
+    public MinecraftHeadsScraper() {}
 
-    private static final String entrypoint = "https://minecraft-heads.com/custom-heads";
 
-    public static ArrayList<String> doSomething(Logger logger) {
-        logger.info(String.format("Start scraping %s", base));
+    @Override
+    public List<Head> scrape(Logger logger) throws IOException {
+        List<Head> heads = new ArrayList<>();
 
-        ArrayList<String> uuids = new ArrayList<>();
+        boolean foundHeads;
+        int startIndex = 0;
 
-        try {
-            Document doc = Jsoup.connect(entrypoint).get();
+        logger.info("Scraping Minecraft-Heads.com started!");
 
-            Elements heads = doc.select(".itemList a");
+        do {
+            foundHeads = false;
 
-            for (Element head : heads) {
-                Document headPage = Jsoup.connect(String.format("%s%s", base, head.attr("href"))).get();
-                Element uuidField = headPage.selectFirst("#UUID-Value");
-                uuids.add(uuidField.text());
+            String overviewURL = String.format("https://minecraft-heads.com/custom-heads?limitstart=%d", startIndex);
+            Document overviewPage = Jsoup.connect(overviewURL).get();
+
+            Elements headLinks = overviewPage.select(".itemList a");
+
+            for (Element headLink: headLinks) {
+                foundHeads = true;
+
+                String detailURL = String.format("https://minecraft-heads.com%s", headLink.attr("href"));
+                Connection connection = Jsoup.connect(detailURL);
+                Connection.Response response = connection.ignoreHttpErrors(true).execute();
+
+                if (response.statusCode() != 200) {
+                    logger.warning(String.format("%d", response.statusCode()));
+                    logger.warning(response.statusMessage());
+
+                    logger.info(detailURL);
+                    continue;
+                }
+
+                Document detailPage = connection.get();
+
+                String name = detailPage.title();
+                String uuid = detailPage.selectFirst("#UUID-Value").text();
+                String category = detailPage.selectFirst("span:contains(Category:)").nextElementSibling().text();
+
+                heads.add(new Head(name, uuid, UUID.randomUUID().toString(), category, new ArrayList<String>(), source, detailURL));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        logger.info(String.format("Done scraping %s", base));
+            logger.info(String.format("Found %d heads so far...", heads.size()));
 
-        return uuids;
+            startIndex += 80;
+        } while (foundHeads && startIndex < 80);
+
+        logger.info("Scraping Minecraft-Heads.com done!");
+
+        return heads;
     }
-
-
 }
--- src/main/java/com/tweska/sweetheads/scraper/Scraper.java
+++ src/main/java/com/tweska/sweetheads/scraper/Scraper.java
@@ -1,4 +1,11 @@
 package com.tweska.sweetheads.scraper;
 
+import com.tweska.sweetheads.heads.Head;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
 public interface Scraper {
+    public List<Head> scrape(Logger logger) throws IOException;
 }
